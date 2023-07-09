a7d31aaa8c27bdab0ce88b8536f84cb43aa3e0a9
perplexingcode <perplexingcode@gmail.com>
2023-07-09 19:16:35 +0700

    Update | db.js: obmitted app name in db name

--- static/db.js
+++ static/db.js
@@ -1,16 +1,52 @@
-import { request } from './request.js';
-import { deepClone } from './utils.js';
+import { request } from "./request.js";
+import { deepClone } from "./utils.js";
+
+function rename(name) {
+  return name.includes("*") ? name.replace("*", "") : "management_" + name;
+}
 
 export const upsert = async function (table, data) {
   const { backendUrl } = useRuntimeConfig();
+  table = rename(table);
   data = deepClone(data);
   delete data.state;
   let response = await request(
-    backendUrl + '/upsert/' + table,
-    'POST',
-    JSON.stringify(data)
+    backendUrl + "/upsert/" + table,
+    "POST",
+    JSON.stringify(data),
   );
   return response;
+};
+
+export const getAll = async function (table) {
+  const { backendUrl } = useRuntimeConfig();
+  table = rename(table);
+  let response = await request(backendUrl + "/all/" + table, "GET");
+  return response;
+};
+
+export const query = async function (table, queryName, queryValue) {
+  const { backendUrl } = useRuntimeConfig();
+  table = rename(table);
+  if (typeof queryValue === "object") {
+    let response = await request(
+      backendUrl + "/query/" + table + "/" + queryName,
+      "POST",
+      JSON.stringify(queryValue),
+    );
+    return response;
+  }
+  let response = await request(
+    backendUrl + "/query/" + table + "/" + queryName + "/" + queryValue,
+    "GET",
+  );
+
+  return response;
+};
+
+export const getById = async function (table, id) {
+  table = rename(table);
+  return await request("/id/" + table + "/" + id, "GET");
 };
 
 // export const deleteItems = async function (id, itemArray, hasState) {
