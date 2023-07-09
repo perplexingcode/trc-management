6d8211a838f3d4f9beb9bcca1eb61afd9e218ca5
perplexingcode <perplexingcode@gmail.com>
2023-07-09 11:51:06 +0700

    Feature | Rerender component

---
+++ components/effect/Rerender.vue
@@ -0,0 +1,18 @@
+<template>
+  <div v-if="display">
+    <slot></slot>
+  </div>
+</template>
+<script setup>
+const display = ref(true);
+async function rerender() {
+  display.value = !display.value;
+  await nextTick();
+  display.value = !display.value;
+}
+
+defineExpose({
+  rerender,
+});
+</script>
+<style></style>
