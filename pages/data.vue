c9a00289df1e3f6f9567075c828c2e624d317228
perplexingcode <perplexingcode@gmail.com>
2023-07-09 19:09:24 +0700

    Feature | Page Data: viewing data by date

--- pages/data.vue
+++ pages/data.vue
@@ -1,11 +1,36 @@
 <template>
-  <Table
-    rows="moves"
-    columns="moveColumns"
-    item-name="move"
-    :dev="false"
-    addRow="true"
-  />
+  <input v-model="date" />
+  <button @click="loadData()">Load Data</button>
+  <EffectRerender ref="dataTable">
+    <Table
+      v-if="isLoadedData"
+      rows="dataRows"
+      columns="moveColumns"
+      item-name="move"
+      addRow="true"
+    />
+  </EffectRerender>
 </template>
-<script setup></script>
+<script setup>
+import { getAll, query } from "~~/static/db";
+
+const isLoadedData = ref(false);
+const date = ref("");
+const dataTable = ref(null);
+let rows = ref(null);
+const loadData = async () => {
+  if (date.value === "") {
+    rows.value = (await getAll("move")).data.value;
+    dataTable.value.rerender();
+    isLoadedData.value = true;
+    return;
+  }
+  await nextTick();
+  rows.value = (await query("move", "date", date)).data.value;
+  dataTable.value.rerender();
+  isLoadedData.value = true;
+};
+
+provide("dataRows", rows);
+</script>
 <style></style>
