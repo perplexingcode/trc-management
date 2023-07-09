c0b9920025dc34f68a3ad11c96a9b61befa84b49
perplexingcode <perplexingcode@gmail.com>
2023-07-09 11:24:39 +0700

    refactor app.vue

--- app.vue
+++ app.vue
@@ -1,7 +1,14 @@
 <template>
   <main class="main_wrap">
     <h1>This is Management</h1>
-    <p class="text-center">Last action: {{ lastActionTimestamp }}</p>
+    <p class="text-center">
+      Last actions:
+      {{
+        JSON.stringify(actionTimestamps?.stack)
+          .replace(/["\[\]]/g, "")
+          .replaceAll(",", ", ")
+      }}
+    </p>
     <nav>
       <ul>
         <li><NuxtLink to="/">Home</NuxtLink></li>
@@ -24,9 +31,6 @@
         </li>
         <li class="menu-parent-item">
           <NuxtLink to="/report">Report</NuxtLink>
-          <ul class="menu-drop-down">
-            <li><NuxtLink to="/report/waste-chore">Waste-Chore</NuxtLink></li>
-          </ul>
         </li>
       </ul>
     </nav>
@@ -34,112 +38,92 @@
   </main>
 </template>
 <script setup>
-import moment from 'moment';
+import moment from "moment";
+import { getAll, query } from "./static/db";
+import FILOArray from "./static/class/FILOArray";
 
+const timeArray = new FILOArray();
+timeArray.maxSize = 5;
+timeArray.push(moment(new Date()).format("HH:mm"));
+const actionTimestamps = ref(timeArray);
 if (process.client) {
-  const lastActionTimestamp = ref(moment(new Date()).format('HH:mm:ss'));
-  var elements = document.getElementsByTagName('*');
+  var elements = document.getElementsByTagName("*");
   for (var i = 0; i < elements.length; i++) {
     // Attach a click event handler to each element
-    elements[i].addEventListener('click', function (event) {
+    elements[i].addEventListener("click", function (event) {
       // Prevent the default behavior of the click event
-      lastActionTimestamp.value = moment(new Date()).format('HH:mm:ss');
+      actionTimestamps.value.push(moment(new Date()).format("HH:mm"));
     });
-    elements[i].addEventListener('change', function (event) {
+    elements[i].addEventListener("change", function (event) {
       // Prevent the default behavior of the click event
-      lastActionTimestamp.value = moment(new Date()).format('HH:mm:ss');
+      actionTimestamps.value.push(moment(new Date()).format("HH:mm"));
     });
   }
 }
-const today = moment(new Date()).format('YYYY-MM-DD');
+const today = moment(new Date()).format("YYYY-MM-DD");
 
-const { backendUrl } = useRuntimeConfig();
-
-const moves = (await useFetch(backendUrl + '/all/' + 'management_move')).data;
-provide('moves', moves);
-
-const movesToday = (
-  await useFetch(backendUrl + '/query' + '/management_move' + '/date/' + today)
-).data;
-provide('movesToday', movesToday);
-
-const wasteMoves = (
-  await useFetch(backendUrl + '/query' + '/management_waste' + '/date/' + today)
-).data;
-provide('waste', wasteMoves);
-const choreMoves = (
-  await useFetch(backendUrl + '/query' + '/management_chore' + '/date/' + today)
-).data;
-provide('chore', choreMoves);
-
-let projects = (await useFetch(backendUrl + '/all/' + 'management_project'))
-  .data;
-// Sort alphabetically
-projects.value = projects.value.sort((a, b) => (a.name > b.name ? 1 : -1));
-provide('projects', projects);
-const projectNames = projects.value.map((p) => p.name);
-
-const queuedMoves = (
-  await useFetch(backendUrl + '/all/' + 'management_queued-move')
-).data;
-provide('queuedMove', queuedMoves);
-
-const dailyMoves = (
-  await useFetch(backendUrl + '/all/' + 'management_daily-move')
-).data;
-provide('dailyMove', dailyMoves);
+const moves = (await getAll("move")).data;
+const movesToday = (await query("move", "date", today)).data;
+const wasteMoves = (await query("waste", "date", today)).data;
+const choreMoves = (await query("chore", "date", today)).data;
+const queuedMoves = (await getAll("queued-move")).data;
+const dailyMoves = (await getAll("daily-move")).data;
 
 let vars;
-const _vars = (await useFetch(backendUrl + '/all/' + 'management_var')).data
-  ._rawValue;
+const _vars = (await getAll("var")).data._rawValue;
 for (const v of _vars) {
   vars = { ...vars, [v.id]: v.value };
 }
-provide('vars', reactive(vars));
+
+let projects = (await getAll("project")).data;
+// Sort alphabetically
+projects.value = projects.value.sort((a, b) => (a.name > b.name ? 1 : -1));
+const projectNames = projects.value.map((p) => p.name);
+
 const queuedMoveColumns = [
   {
-    name: '',
-    key: 'is-selected',
-    type: 'is-selected',
+    name: "",
+    key: "is-selected",
+    type: "is-selected",
     disabled: true,
     noSave: true,
-    attrs: { type: 'text' },
+    attrs: { type: "text" },
   },
   {
-    name: 'Move',
-    key: 'name',
-    type: 'input-name',
+    name: "Move",
+    key: "name",
+    type: "input-name",
     disabled: false,
-    attrs: { type: 'text', required: true },
+    attrs: { type: "text", required: true },
   },
   {
-    name: '⟁',
-    key: 'weight',
-    type: 'input',
+    name: "⟁",
+    key: "weight",
+    type: "input",
     disabled: false,
-    attrs: { type: 'number' },
+    attrs: { type: "number" },
   },
   {
-    name: 'Done',
-    key: 'done',
-    type: 'checkbox',
+    name: "Done",
+    key: "done",
+    type: "checkbox",
     disabled: false,
     default: false,
-    attrs: { type: 'checkbox' },
+    attrs: { type: "checkbox" },
   },
   {
-    name: 'Priority',
-    key: 'priority',
-    type: 'select',
+    name: "Priority",
+    key: "priority",
+    type: "select",
     disabled: false,
     options: [
-      '1-Urgent',
-      '2-Necessary',
-      '3-Important',
-      '4-Recommended',
-      '5-Optional',
+      "1-Urgent",
+      "2-Necessary",
+      "3-Important",
+      "4-Recommended",
+      "5-Optional",
     ],
-    attrs: { type: 'text' },
+    attrs: { type: "text" },
   },
   // {
   //   name: 'Personnel',
@@ -162,26 +146,24 @@ const queuedMoveColumns = [
   //   ],
   // },
   {
-    name: 'Project',
-    key: 'prj',
-    type: 'select',
+    name: "Project",
+    key: "prj",
+    type: "select",
     disabled: false,
     options: projectNames,
-    attrs: { type: 'text', required: false },
+    attrs: { type: "text", required: false },
   },
 ];
-
-provide('queuedMoveColumns', queuedMoveColumns);
 
 //  #MOVES
 let moveColumns = [
   {
-    name: '',
-    key: 'is-selected',
-    type: 'is-selected',
+    name: "",
+    key: "is-selected",
+    type: "is-selected",
     disabled: true,
     noSave: true,
-    attrs: { type: 'text' },
+    attrs: { type: "text" },
   },
   // {
   //   name: 'ID',
@@ -189,158 +171,202 @@ let moveColumns = [
   //   type: 'p',
   // },
   {
-    name: 'Move',
-    key: 'name',
-    type: 'input-name',
+    name: "Move",
+    key: "name",
+    type: "input-name",
     disabled: false,
-    attrs: { type: 'text', required: true },
+    attrs: { type: "text", required: true },
   },
   {
-    name: 'Done',
-    key: 'done',
-    type: 'checkbox',
+    name: "Done",
+    key: "done",
+    type: "checkbox",
     disabled: false,
     default: false,
-    attrs: { type: 'checkbox' },
+    attrs: { type: "checkbox" },
   },
   {
-    name: 'Description',
-    key: 'des',
-    type: 'text-area',
+    name: "Description",
+    key: "des",
+    type: "text-area",
     disabled: false,
-    attrs: { type: 'text' },
+    attrs: { type: "text" },
   },
   {
-    name: 'Date',
-    key: 'date',
-    type: 'input',
+    name: "Date",
+    key: "date",
+    type: "input",
     disabled: false,
-    default: moment().format('YYYY-MM-DD'),
-    attrs: { type: 'text', required: true, suggestion: false },
+    default: moment().format("YYYY-MM-DD"),
+    attrs: { type: "text", required: true, suggestion: false },
   },
   {
-    name: 'Duration',
-    key: 'duration',
-    type: 'input',
+    name: "Duration",
+    key: "duration",
+    type: "input",
     disabled: false,
-    attrs: { type: 'text', required: true, suggestion: false },
+    attrs: { type: "text", required: true, suggestion: false },
   },
   {
-    name: 'Category',
-    key: 'cat',
-    type: 'select',
+    name: "Category",
+    key: "cat",
+    type: "select",
     disabled: false,
     options: [
-      'Navigation',
-      'Cosmic Engine',
-      'Engineering',
-      'Aesthetics',
-      'Operation',
-      'Business',
+      "Navigation",
+      "Cosmic Engine",
+      "Engineering",
+      "Aesthetics",
+      "Operation",
+      "Business",
     ],
-    attrs: { type: 'text', required: true },
+    attrs: { type: "text", required: true },
   },
   {
-    name: 'Project',
-    key: 'prj',
-    type: 'select',
+    name: "Project",
+    key: "prj",
+    type: "select",
     disabled: false,
     options: projectNames,
-    attrs: { type: 'text', required: true },
+    attrs: { type: "text", required: true },
   },
   {
-    name: 'Group',
-    key: 'grp',
-    type: 'select',
+    name: "Group",
+    key: "grp",
+    type: "select",
     disabled: false,
-    options: ['Personal', 'MFVN', 'Freelace', 'TCGS', 'TrinityLTD'],
-    attrs: { type: 'text', required: true },
+    options: ["Personal", "MFVN", "Freelace", "TCGS", "TrinityLTD"],
+    attrs: { type: "text", required: true },
   },
   {
-    name: 'Tags',
-    key: 'tags',
-    type: 'input',
+    name: "Tags",
+    key: "tags",
+    type: "input",
     disabled: false,
-    attrs: { type: 'text' },
+    attrs: { type: "text" },
   },
 ];
-provide('moveColumns', moveColumns);
 
 const wasteChoreColumns = [
   {
-    name: '',
-    key: 'is-selected',
-    type: 'is-selected',
+    name: "",
+    key: "is-selected",
+    type: "is-selected",
     disabled: true,
-    attrs: { type: 'text' },
+    attrs: { type: "text" },
   },
   {
-    name: 'Duration',
-    key: 'duration',
-    type: 'input',
+    name: "Duration",
+    key: "duration",
+    type: "input",
     disabled: false,
-    attrs: { type: 'text', required: true },
+    attrs: { type: "text", required: true },
   },
   {
-    name: 'Move',
-    key: 'name',
-    type: 'input-name',
+    name: "Move",
+    key: "name",
+    type: "input-name",
     data: choreMoves.value.map((m) => m.name),
     disabled: false,
-    attrs: { type: 'text' },
+    attrs: { type: "text" },
   },
   {
-    name: 'Date',
-    key: 'date',
-    type: 'input',
+    name: "Date",
+    key: "date",
+    type: "input",
     disabled: false,
-    default: moment().format('YYYY-MM-DD'),
-    attrs: { type: 'text' },
+    default: moment().format("YYYY-MM-DD"),
+    attrs: { type: "text" },
   },
 ];
-provide('wasteChoreColumns', wasteChoreColumns);
-
-// const choreMoves = (
-//   await useFetch(backendUrl + '/db/management_chore/all')
-// ).data;
-// provide('chore', choreMoves);
-
-// const wasteMovesToday = wasteMoves.value.filter((move) => {
-//   return move.date === moment().format('YYYY-MM-DD');
-// });
-// provide('wasteToday', wasteMovesToday);
-// const choreMovesToday = choreMoves.filter((move) => {
-//   return move.date === moment().format('YYYY-MM-DD');
-// });
-// provide('choreToday', choreMovesToday);
-//
 
 // #PROJECTS
 const projectColumns = [
   {
-    name: '',
-    key: 'is-selected',
-    type: 'is-selected',
+    name: "",
+    key: "is-selected",
+    type: "is-selected",
     disabled: true,
-    attrs: { type: 'text' },
+    attrs: { type: "text" },
   },
   {
-    name: 'Project',
-    key: 'name',
-    type: 'input',
+    name: "Project",
+    key: "name",
+    type: "input",
     disabled: false,
-    attrs: { type: 'text' },
+    attrs: { type: "text" },
   },
   {
-    name: 'Hour',
-    key: 'hour',
-    type: 'input',
+    name: "Hour",
+    key: "hour",
+    type: "input",
     disabled: false,
-    attrs: { type: 'text' },
+    attrs: { type: "text" },
   },
 ];
 
-provide('projectColumns', projectColumns);
+// #TRANSACTIONS
+
+const transactions = (await getAll("transaction")).data;
+const transactionColumns = [
+  {
+    name: "",
+    key: "is-selected",
+    type: "is-selected",
+    disabled: true,
+    attrs: { type: "text" },
+  },
+  {
+    name: "Category",
+    key: "cat",
+    type: "select",
+    disabled: false,
+    options: [
+      "FoodnBeverage",
+      "Services",
+      "Consumables",
+      "Shopping",
+      "Movement",
+      "Relationship",
+      "Health",
+      "Maintainance",
+      "Education",
+      "Investment",
+      "-",
+    ],
+    attrs: { type: "text", required: true },
+  },
+  {
+    name: "Group",
+    key: "grp",
+    type: "select",
+    disabled: false,
+    options: [
+      "Navigation",
+      "Cosmic Engine",
+      "Engineering",
+      "Aesthetics",
+      "Operation",
+      "Business",
+    ],
+    attrs: { type: "text", required: true },
+  },
+];
+
+// PROVIDE
+provide("moves", moves);
+provide("movesToday", movesToday);
+provide("waste", wasteMoves);
+provide("chore", choreMoves);
+provide("projects", projects);
+provide("queuedMove", queuedMoves);
+provide("dailyMove", dailyMoves);
+provide("vars", reactive(vars));
+provide("queuedMoveColumns", queuedMoveColumns);
+provide("moveColumns", moveColumns);
+provide("wasteChoreColumns", wasteChoreColumns);
+provide("projectColumns", projectColumns);
+provide("transactions", transactions);
 
 // Ultilities
 Array.prototype.random = function () {
