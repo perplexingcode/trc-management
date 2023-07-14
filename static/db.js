d1933346933819f646825ad412e23c568172968d
perplexingcode <perplexingcode@gmail.com>
2023-07-14 16:26:02 +0700

    Feature:(Request) - Adđ projection

--- static/db.js
+++ static/db.js
@@ -6,46 +6,62 @@ function rename(name) {
 }
 
 export const upsert = async function (table, data) {
-  const { backendUrl } = useRuntimeConfig();
   table = rename(table);
   data = deepClone(data);
   delete data.state;
   let response = await request(
-    backendUrl + "/upsert/" + table,
+    "/upsert/" + table,
     "POST",
     JSON.stringify(data),
   );
   return response;
 };
 
-export const getAll = async function (table) {
-  const { backendUrl } = useRuntimeConfig();
+export const getAll = async function (table, projection) {
   table = rename(table);
-  let response = await request("all" + "/" + table, "GET");
+  let response = await request(
+    "all" + "/" + table + (projection ? "/" + projection : ""),
+    "GET",
+  );
   return response;
 };
 
-export const query = async function (table, queryName, queryValue) {
+export const query = async function (table, queryName, queryValue, projection) {
   table = rename(table);
   if (typeof queryValue === "object") {
     let response = await request(
-      "query" + "/" + table + "/" + queryName,
+      "query" +
+        "/" +
+        table +
+        "/" +
+        queryName +
+        (projection ? "/" + projection : ""),
       "POST",
       JSON.stringify(queryValue),
     );
     return response;
   }
   let response = await request(
-    "query" + "/" + table + "/" + queryName + "/" + queryValue,
+    "query" +
+      "/" +
+      table +
+      "/" +
+      queryName +
+      "/" +
+      queryValue +
+      (projection ? "/" + projection : ""),
     "GET",
   );
 
   return response;
 };
 
-export const getById = async function (table, id) {
+export const getById = async function (table, id, projection) {
   table = rename(table);
-  return await request("id" + "/" + table + "/" + id, "GET");
+  return await request(
+    "id" + "/" + table + "/" + id + (projection ? "/" + projection : ""),
+    "GET",
+  );
 };
 
 export const dbDelete = async function (table, list) {
