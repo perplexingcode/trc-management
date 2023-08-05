239937fa543abdeda6834a640a3295aec571266c
perplexingcode <perplexingcode@gmail.com>
2023-08-05 08:34:42 +0700

    Design | ButtonShowHide: Change icon to svg

--- components/BtnShowHide.vue
+++ components/BtnShowHide.vue
@@ -1,6 +1,28 @@
 <template>
   <button @click="isShow = !isShow" class="btn-circle">
-    {{ isShow ? '-' : '+' }}
+    <svg
+      v-if="isShow"
+      class="w-3 h-3"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="3"
+    >
+      <line x1="5" y1="12" x2="19" y2="12" stroke-linecap="round"></line>
+    </svg>
+    <svg
+      v-else
+      class="w-3 h-3"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="3"
+    >
+      <line x1="12" y1="5" x2="12" y2="19" stroke-linecap="round"></line>
+      <line x1="5" y1="12" x2="19" y2="12" stroke-linecap="round"></line>
+    </svg>
   </button>
 </template>
 <script setup>
