bc644b542be97180bd55c510391c4c793a615637
perplexingcode <perplexingcode@gmail.com>
2023-08-05 08:33:54 +0700

    Feature | Note: - Rename note, add & change note title

--- components/Note.vue
+++ components/Note.vue
@@ -1,66 +1,89 @@
 <template>
   <div class="note flex flex-col h-fit">
-    <h3>{{ title }}</h3>
-    <textarea
-      onfocus='this.style.height = "";this.style.height = this.scrollHeight + "px"'
-      v-model="note.text"
-      :class="{ isEditing }"
-      @input="isEditing = true"
-      @keydown="handleKeydown($event)"
-      @blur="applyChange()"
-    />
-
-    <div class="version-history">
-      <div class="flex justify-between">
-        <button v-show="showVersion" @click="viewVersion == 0 || viewVersion--">
-          &lt;
-        </button>
-        <btn-show-hide
-          v-if="props.showVersionHistory"
-          class="m-auto"
-          @click="showVersion = !showVersion"
-          :is-default-show="false"
-        />
-        <button
-          v-show="showVersion"
-          @click="
-            viewVersion == note.versionHistory.stack.length - 1 || viewVersion++
-          "
-        >
-          &gt;
-        </button>
-      </div>
-      <div v-show="showVersion" class="flex p-2">
-        <div
-          v-for="(item, index) in note.versionHistory.stack"
-          :key="item.id"
-          class="w-4 h-4 bg-gray-500"
-          :class="{
-            'bg-yellow-500': index == viewVersion,
-            'mr-2': index != note.versionHistory.stack.length - 1,
-          }"
-        ></div>
-      </div>
-      <textarea
-        v-show="showVersion"
-        :value="note.versionHistory.stack[viewVersion]?.text"
-        disabled
+    <div class="note-main rounded">
+      <h3 v-show="!state.showEditTitle" @dblclick="changeTitle">
+        {{ note.title }}
+      </h3>
+      <input
+        v-show="state.showEditTitle"
+        v-model="newTitle"
+        @keydown.enter="updateTitle"
       />
-      <p
-        v-show="
-          showVersion && note.versionHistory.stack[viewVersion]?.timestamp
-        "
-        class="text-xs text-center"
-      >
-        {{ note.versionHistory.stack[viewVersion]?.timestamp }}
-      </p>
+      <textarea
+        onfocus='this.style.height = "";this.style.height = this.scrollHeight + "px"'
+        v-model="note.text"
+        :class="{ isEditing: state.isEditing }"
+        @input="state.isEditing = true"
+        @keydown="handleKeydown($event)"
+        @blur="applyChange()"
+      />
+    </div>
+    <div class="note-detail" v-if="props.showDetail">
+      <btn-show-hide
+        class="m-auto"
+        @click="state.showMore = !state.showMore"
+        :is-default-show="false"
+      />
+      <div class="detail" v-show="state.showMore">
+        <div class="note-action flex justify-center">
+          <button @click="changeNoteName">Rename</button>
+          <button v-if="!note.title" @click="state.showEditTitle = true">
+            Add title
+          </button>
+        </div>
+        <div class="flex justify-between">
+          <button
+            v-show="state.showMore"
+            @click="state.viewVersion == 0 || state.viewVersion--"
+            class="rounded-full w-8 h-8"
+          >
+            &lt;
+          </button>
+
+          <button
+            v-show="state.showMore"
+            @click="
+              state.viewVersion == note.versionHistory.stack.length - 1 ||
+                state.viewVersion++
+            "
+            class="rounded-full w-8 h-8"
+          >
+            &gt;
+          </button>
+        </div>
+        <div v-show="state.showMore" class="flex p-2 items-center">
+          <div
+            v-for="(item, index) in note.versionHistory.stack"
+            :key="item.id"
+            class="w-4 h-4 bg-gray-500"
+            :class="{
+              'bg-yellow-500': index == state.viewVersion,
+              'mr-2': index != note.versionHistory.stack.length - 1,
+            }"
+          ></div>
+        </div>
+        <textarea
+          v-show="state.showMore"
+          :value="note.versionHistory.stack[state.viewVersion]?.text"
+          disabled
+        />
+        <p
+          v-show="
+            state.showMore &&
+            note.versionHistory.stack[state.viewVersion]?.timestamp
+          "
+          class="text-xs text-center"
+        >
+          {{ note.versionHistory.stack[state.viewVersion]?.timestamp }}
+        </p>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
 import { v4 } from 'uuid';
-import { upsert } from '~~/static/db';
+import { dbDelete, upsert } from '~~/static/db';
 import { createTimestamp } from '~~/static/time';
 import { syncReactive } from '~~/static/utils';
 import FILOArray from '~~/static/class/FILOArrayNote';
@@ -75,13 +98,8 @@ const props = defineProps({
     type: String,
     required: false,
   },
-  title: {
-    type: String,
-    required: false,
-  },
-  showVersionHistory: {
+  showDetail: {
     type: Boolean,
-    required: false,
     default: true,
   },
   data: {
@@ -90,8 +108,12 @@ const props = defineProps({
   },
 });
 
-const showVersion = ref(false);
-const viewVersion = ref(0);
+const state = reactive({
+  isEditing: false,
+  showMore: false,
+  viewVersion: 0,
+  showEditTitle: false,
+});
 
 const note = reactive({
   id: v4(),
@@ -99,8 +121,20 @@ const note = reactive({
   lastUpdated: '',
   box: props.box,
   name: props.name,
+  title: '',
   versionHistory: new FILOArray(),
 });
+
+const newTitle = ref(note?.title || '');
+
+function changeTitle() {
+  state.showEditTitle = true;
+}
+function updateTitle() {
+  state.showEditTitle = false;
+  note.title = newTitle.value;
+  upsert('note', note);
+}
 
 const { backendUrl } = useRuntimeConfig();
 onMounted(async () => {
@@ -109,11 +143,11 @@ onMounted(async () => {
   if (props.data) {
     syncReactive(note, props.data);
     Object.setPrototypeOf(note.versionHistory, FILOArray.prototype);
-    viewVersion.value = note.versionHistory.stack.length - 1;
+    state.viewVersion = note.versionHistory.stack.length - 1;
     return;
   }
   let cloudNote = await useFetch(
-    backendUrl + '/query' + '/management_note' + '/name/' + props.name
+    backendUrl + '/query' + '/management_note' + '/name/' + props.name,
   );
   cloudNote = cloudNote.data._rawValue?.[0];
   // if (!cloudNote) {
@@ -129,7 +163,7 @@ onMounted(async () => {
   try {
     syncReactive(note, cloudNote);
     Object.setPrototypeOf(note.versionHistory, FILOArray.prototype);
-    viewVersion.value = note.versionHistory.stack.length - 1;
+    state.viewVersion = note.versionHistory.stack.length - 1;
     // wait 0.5s to make sure all notes are loaded
     await new Promise((resolve) => setTimeout(resolve, 500));
   } catch (error) {
@@ -137,11 +171,10 @@ onMounted(async () => {
   }
 });
 
-const isEditing = ref(false);
 // Alert save changes
 if (process.client) {
   window.addEventListener('beforeunload', function (event) {
-    if (isEditing.value) {
+    if (state.isEditing) {
       event.preventDefault();
       event.returnValue = '';
       return '';
@@ -156,8 +189,7 @@ const handleKeydown = (event) => {
 };
 
 const applyChange = () => {
-  console.log(note);
-  if (note.text == note.versionHistory[viewVersion]) {
+  if (note.text == note.versionHistory[state.viewVersion]) {
     return;
   }
   upsert('note_backup', {
@@ -170,9 +202,19 @@ const applyChange = () => {
   note.lastUpdated = createTimestamp();
   note.versionHistory.push(note.text);
   upsert('note', note);
-  isEditing.value = false;
-  viewVersion.value = note.versionHistory.stack.length - 1;
+  state.isEditing = false;
+  state.viewVersion = note.versionHistory.stack.length - 1;
 };
+
+function changeNoteName() {
+  const newName = prompt('Please enter new name', note.name);
+  // if (newName == null || newName == '') {
+  //   return;
+  // }
+  note.name = newName;
+  dbDelete('note', note.id);
+  upsert('note', note);
+}
 </script>
 
 <style scoped>
