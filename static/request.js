2b78d7c40a83f163308e9a59b77de7b00ec4e04d
perplexingcode <perplexingcode@gmail.com>
2023-08-17 22:13:25 +0700

    null

--- static/request.js
+++ static/request.js
@@ -4,17 +4,17 @@ export async function request(path, method, data, header, attr) {
     method: method,
     body: data,
     headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + attr?.token || sessionToken?.value,
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ' + attr?.token || sessionToken?.value,
     },
     data: data,
   };
   if (header) {
     config.headers = { ...config.headers, ...header };
   }
-  if (method === "GET") delete config.body;
-  if (attr?.noCors) config.mode = "no-cors";
-  if (!path.includes("http")) path = backendUrl + "/" + path;
+  if (method === 'GET') delete config.body;
+  if (attr?.noCors) config.mode = 'no-cors';
+  if (!path.includes('http')) path = backendUrl + '/' + path;
   return await useFetch(path, config);
 }
 
