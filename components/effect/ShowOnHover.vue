62e10116907ce279e3f3db730074e1e7d5c7de6b
perplexingcode <perplexingcode@gmail.com>
2023-08-26 05:04:08 +0700

    Faeture | Effect: Show on hover

---
+++ components/effect/ShowOnHover.vue
@@ -0,0 +1,56 @@
+<template>
+  <div
+    @mouseover="showTime = props.time"
+    @mouseleave="showTemp()"
+    ref="wrapper"
+    :class="id"
+  >
+    <component is="style">
+      .{{ id }} .can-toggle { display: {{ showTime ? 'block' : 'none' }}; }
+    </component>
+    <slot></slot>
+  </div>
+</template>
+<script setup>
+import { v4 } from 'uuid';
+import { onClickOutside } from '@vueuse/core';
+
+const props = defineProps({
+  time: {
+    type: Number,
+    default: 1500,
+  },
+  interval: {
+    type: Number,
+    default: 500,
+  },
+  forceHide: {
+    type: Boolean,
+    default: false,
+  },
+});
+const wrapper = ref(null);
+const showTime = ref(0);
+let showInterval = null;
+
+let id = ref(null);
+onMounted(async () => {
+  if (!id.value) id.value = 's' + v4();
+  await nextTick();
+});
+
+// Handle dropdown menu
+onClickOutside(wrapper, () => (showTime.value = 0));
+
+function showTemp() {
+  if (showInterval) clearInterval(showInterval);
+  showInterval = setInterval(() => {
+    showTime.value = showTime.value - props.interval;
+    if (showTime.value < 0) {
+      showTime.value = 0;
+      clearInterval(showInterval);
+    }
+  }, props.interval);
+}
+</script>
+<style></style>
